0fba360e549fb0c5ca6662ec297ed4eed120ff94
cscites <cscites@gmail.com>
2017-05-23 04:51:23 -0400

    2017-05-23

---
+++ src/com/ho8c/LeadSetCreator.java
@@ -0,0 +1,55 @@
+package com.ho8c;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Scanner;
+
+/**
+ * Created by chris on 5/23/17.
+ */
+public class LeadSetCreator {
+    HashSet<Lead>leads = new HashSet<>();
+    public LeadSetCreator(String source) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(source));
+        scanner.useDelimiter(",");
+        scanner.nextLine();
+        while(scanner.hasNextLine()){
+            String showSource = scanner.next();
+            String specialty = scanner.next();
+            String conference = scanner.next();
+            String date = scanner.next();
+            String prefix = scanner.next();
+            String firstName = scanner.next();
+            String middleName = scanner.next();
+            String lastName = scanner.next();
+            String degree = scanner.next();
+            String company = scanner.next();
+            String address1 = scanner.next();
+            String address2 = scanner.next();
+            String city = scanner.next();
+            String state = scanner.next();
+            String zip = scanner.next();
+            String country = scanner.next();
+            String phone = scanner.next();
+            String fax = scanner.next();
+            String email = scanner.next();
+            String notes = scanner.next();
+            String geoInt = scanner.next();
+            String availability = scanner.next();
+            String status = scanner.next();
+            Lead candidate = new Lead(showSource, specialty, conference, date, prefix, firstName, middleName,
+                    lastName, degree, company, address1, address2, city, state, zip, country, phone, fax,
+                    email, notes, geoInt, availability, status);
+            leads.add(candidate);
+            scanner.nextLine();
+
+        }
+        scanner.close();
+    }
+
+
+    public HashSet getLeadSet(){
+        return leads;
+    }
+}
--- src/com/ho8c/Main.java
+++ src/com/ho8c/Main.java
@@ -1,81 +1,19 @@
 package com.ho8c;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashSet;
-import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args)throws FileNotFoundException{
-        String source;
-        String specialty;
-        String conference;
-        String date;
-        String prefix;
-        String firstName;
-        String middleName;
-        String lastName;
-        String degree;
-        String company;
-        String address1;
-        String address2;
-        String city;
-        String state;
-        String zip;
-        String country;
-        String phone;
-        String fax;
-        String email;
-        String notes;
-        String geoInt;
-        String availability;
-        String status;
-        HashSet<Lead>leads = new HashSet<>();
-        Scanner scanner = new Scanner(new File("/home/chris/Desktop/AAN 2017/leads.csv"));
-        scanner.useDelimiter(",");
-        scanner.nextLine();
-        while(scanner.hasNextLine()){
-            source = scanner.next();
-            specialty = scanner.next();
-            conference = scanner.next();
-            date = scanner.next();
-            prefix = scanner.next();
-            firstName = scanner.next();
-            middleName = scanner.next();
-            lastName = scanner.next();
-            degree = scanner.next();
-            company = scanner.next();
-            address1 = scanner.next();
-            address2 = scanner.next();
-            city = scanner.next();
-            state = scanner.next();
-            zip = scanner.next();
-            country = scanner.next();
-            phone = scanner.next();
-            fax = scanner.next();
-            email = scanner.next();
-            notes = scanner.next();
-            geoInt = scanner.next();
-            availability = scanner.next();
-            status = scanner.next();
-            Lead candidate = new Lead(source, specialty, conference, date, prefix, firstName, middleName,
-                    lastName, degree, company, address1, address2, city, state, zip, country, phone, fax,
-                    email, notes, geoInt, availability, status);
-            leads.add(candidate);
-            scanner.nextLine();
+    public static void main(String[] args) throws FileNotFoundException {
+        HashSet<Lead>leads = new LeadSetCreator("/home/chris/Desktop/AAN 2017/leads.csv").getLeadSet();
 
-        }
-        scanner.close();
 
         for (Lead lead: leads) {
             System.out.print(lead.getPrefix() + " ");
             System.out.print(lead.getFirstName() + " ");
             System.out.println(lead.getLastName());
         }
-
-
-
     }
 }
 
