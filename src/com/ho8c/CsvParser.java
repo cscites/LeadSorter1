87a5c019d6351466b163b8de6e16d70750eb55ba
Chris Scites <cscites@gmail.com>
2017-06-05 15:36:41 -0400

    2017-06-05

--- src/com/ho8c/CsvParser.java
+++ src/com/ho8c/CsvParser.java
@@ -39,7 +39,6 @@ public class CsvParser extends ArrayList<String[]> {
                     }
                     variables[n] = string.toString();
                     n--;
-
                 } else if (string.substring(string.length() - 1).matches("\"")) {
                     string.setLength(string.lastIndexOf("\""));
                     variables[n] = string.substring(string.lastIndexOf("\"") + 1, string.length()).trim();
